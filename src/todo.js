824888588a7ecb3710b87bf2368d6c2e3139cd88
kkata <redprin900@yahoo.co.jp>
2018-07-12 14:48:10 +0900

    React Todo List Example (Filtering Todos)

--- src/todo.js
+++ src/todo.js
@@ -61,9 +61,58 @@ const store = createStore(todoApp)
 
 const { Component } = React
 
+const FilterLink = ({
+	filter,
+	currentFilter,
+	children
+}) => {
+	if (filter === currentFilter) {
+		return <span>{children}</span>
+	}
+	return (
+		<a href='#'
+			onClick={e => {
+				e.preventDefault()
+			 		store.dispatch({
+			 			type: 'SET_VISIBILITY_FILTER',
+			 			filter
+			 	})
+			}}
+		>
+			{children}
+		</a>
+	)
+}
+
+const getVisibleTodos = (
+	todos,
+	filter
+) => {
+	switch (filter) {
+		case 'SHOW_ALL':
+			return todos
+		case 'SHOW_COMPLETED':
+			return todos.filter(
+				t => t.completed
+			)
+		case 'SHOW_ACTIVE':
+			return todos.filter(
+				t => !t.completed
+			)
+	}
+}
+
 let nextTodoId = 0
 class TodoApp extends Component {
 	render() {
+		const {
+			todos,
+			visibilityFilter
+		} = this.props
+		const visibleTodos = getVisibleTodos(
+			todos,
+			visibilityFilter
+		)
 		return (
 			<div>
 				<input ref={node => {
@@ -80,7 +129,7 @@ class TodoApp extends Component {
 					Add Todo
 				</button>
 				<ul>
-					{this.props.todos.map(todo =>
+					{visibleTodos.map(todo =>
 						<li key={todo.id}
 								onClick={() => {
 									store.dispatch({
@@ -96,6 +145,30 @@ class TodoApp extends Component {
 						</li>
 					)}
 				</ul>
+				<p>
+					Show:
+					{' '}
+					<FilterLink
+						filter='SHOW_ALL'
+						currentFilter={visibilityFilter}
+					>
+						All
+					</FilterLink>
+					{' '}
+					<FilterLink
+						filter='SHOW_ACTIVE'
+						currentFilter={visibilityFilter}
+					>
+						Active
+					</FilterLink>
+					{' '}
+					<FilterLink
+						filter='SHOW_COMPLETED'
+						currentFilter={visibilityFilter}
+					>
+						Completed
+					</FilterLink>
+				</p>
 			</div>
 		)
 	}
@@ -104,7 +177,7 @@ class TodoApp extends Component {
 const render = () => {
 	ReactDom.render(
 		<TodoApp
-			todos={store.getState().todos}
+			{...store.getState()}
 		/>,
 		document.getElementById('root')
 	)
